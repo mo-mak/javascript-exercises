1bb19c32c51aed233dddc3d23cdf054e291ced5c
momak <moh.makarehchi@gmail.com>
2023-03-11 12:16:58 -0500

    Add solution to exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatNum) {
+    
+    let newString = '';
+    
+    if (repeatNum < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < repeatNum; i++) {
+            newString += string;   
+        }
+    }
+    
+    return newString;
 };
+
+// console.log(repeatString('Hello', 5));
 
 // Do not edit below this line
 module.exports = repeatString;
