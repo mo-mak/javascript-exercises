247f29b1b6e8daaf2d68c3a1b01f64ccb5ccbb8b
momak <moh.makarehchi@gmail.com>
2023-03-11 12:45:25 -0500

    Add solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseStr = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseStr += string.charAt(i);   
+    };
+    return reverseStr;
 
 };
 
