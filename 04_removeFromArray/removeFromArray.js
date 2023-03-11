93136ff8e76beee7bbdad93b3e998f3b9c0b5340
momak <moh.makarehchi@gmail.com>
2023-03-11 18:53:03 -0500

    Add solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToRmv) {
+    
+    for (const item of itemsToRmv) {
+        if (array.includes(item)) {
+            const index = array.indexOf(item);
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
+
+// removeFromArray([3, 5, 4, 6], 3)
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
