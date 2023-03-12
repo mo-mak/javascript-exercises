24c75fedb674ca85fc4bf7d8f388d6b6d4f365ff
momak <moh.makarehchi@gmail.com>
2023-03-11 19:42:55 -0500

    Add exercise 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    
+    if (startNum > endNum) {
+        const temp = startNum;
+        startNum = endNum;
+        endNum = temp;
+    }
+    
+    if (startNum < 0 || endNum < 0) {
+        return 'ERROR';
+    }
 
+    if (typeof startNum != 'number' || typeof endNum != 'number') {
+        return 'ERROR'
+    }
+
+    for (let i = endNum; i >= startNum; i--) {
+        sum += i;
+    }
+    return sum;
 };
+
+// console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
